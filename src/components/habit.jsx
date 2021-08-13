0635a669396624c092f157f37c2a4ea8e49a56f9
jsfolio <jhm8540@daum.net>
2021-08-13 19:33:06 +0900

    0813

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -5,13 +5,7 @@ class Habit extends Component {
         count: 0,
     }
     handleIncrement = (e) => {
-        // setState에 객체를 전달하는 것과 함수를 전달하는 것은 어떤 차이가 있나요?
-            // updater 함수를 전달하면 updater 함수 안에서 이전 state 값에 접근할 수 있습니다.
-            // setState 호출은 일괄적으로 처리되기 때문에
-            // 여러 업데이트 사항이 충돌 없이 차례대로 반영되도록 합니다.
         this.setState({
-            // 중요: 값을 업데이트할 때 `this.state` 대신 `state` 값을 읽어옵니다.
-
             count: this.state.count+1
         })
     }
